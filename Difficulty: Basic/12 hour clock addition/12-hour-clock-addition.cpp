f3925df87525fc919294290644c89a36c36d500f
Santhosh <124782185+SanthoshK2003@users.noreply.github.com>
2025-02-12 22:15:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/12 hour clock addition/12-hour-clock-addition.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int clockSum(int num1, int num2){
+        // code here
+        int ans=num1+num2;
+        return ans%12;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int num1, num2;
+        cin>>num1>>num2;
+        
+        Solution ob;
+        cout<<ob.clockSum(num1, num2)<<endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
